17f904378f7b7fa1d32b74dbf435096d034efb10
sherifzaher <sherifashrafmedia@gmail.com>
2023-07-23 09:01:18 +0300

    render free-counter in sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,6 +7,7 @@ import {usePathname} from "next/navigation";
 
 import {cn} from "@/lib/utils";
 import {Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon} from "lucide-react";
+import FreeCounter from "@/components/free-counter";
 
 const montserrat = Montserrat({ weight: "600", subsets:['latin'] });
 
@@ -54,29 +55,36 @@ const routes = [
   }
 ]
 
-export default function Sidebar(){
+type Props = {
+  apiLimitCount: number;
+}
+
+export default function Sidebar({ apiLimitCount }: Props){
   const pathname = usePathname();
 
   return (
       <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-          <div className="px-3 py-2 flex-1">
-            <Link href={'/dashboard'} className="flex items-center pl-3 mb-14">
-              <div className="relative w-8 h-8 mr-4">
-                <Image src={"/logo.png"} alt="" fill />
-              </div>
-              <h1 className={cn("text-2xl font-bold", montserrat.className)}>Artifix</h1>
-            </Link>
-            <div className="space-y-1">
-              {routes.map((route) => (
-                <Link href={route.href} key={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
-                  <div className="flex items-center flex-1">
-                    <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
-                    {route.label}
-                  </div>
-                </Link>
-              ))}
+        <div className="px-3 py-2 flex-1">
+          <Link href={'/dashboard'} className="flex items-center pl-3 mb-14">
+            <div className="relative w-8 h-8 mr-4">
+              <Image src={"/logo.png"} alt="" fill />
             </div>
+            <h1 className={cn("text-2xl font-bold", montserrat.className)}>Artifix</h1>
+          </Link>
+          <div className="space-y-1">
+            {routes.map((route) => (
+              <Link href={route.href} key={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
+                <div className="flex items-center flex-1">
+                  <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                  {route.label}
+                </div>
+              </Link>
+            ))}
           </div>
+        </div>
+        <FreeCounter
+          apiLimitCount={apiLimitCount}
+        />
       </div>
   )
 }
