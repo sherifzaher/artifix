c164636e2c5df3e5086e3c8335d1e10f86526166
sherifzaher <sherifashrafmedia@gmail.com>
2023-07-23 08:36:38 +0300

    apply apiLimit on all endpoints /api/*

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 import {NextResponse} from "next/server";
 import {auth} from "@clerk/nextjs";
+import {checkApiLimit, increaseApiLimit} from "@/lib/api-limit";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
@@ -37,11 +38,19 @@ export async function POST(
       return new NextResponse("Resolution are required", { status: 400 })
     }
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free Trial has expired", { status: 403 })
+    }
+
     const response = await openai.createImage({
       prompt,
       n: parseInt(amount, 10),
       size: resolution
     });
+
+    await increaseApiLimit();
 
     return NextResponse.json(response.data.data);
 
