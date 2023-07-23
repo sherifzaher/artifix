83b9e02c2d24704e661b078e251bc9d8f206f34e
sherifzaher <sherifashrafmedia@gmail.com>
2023-07-23 07:05:12 +0300

    implement code generation and it's point + call the endpoint /api/code

---
+++ app/api/code/route.ts
@@ -0,0 +1,48 @@
+import {ChatCompletionRequestMessage, Configuration, OpenAIApi} from 'openai';
+import {NextResponse} from "next/server";
+import {auth} from "@clerk/nextjs";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: "system",
+  content: "You are a code generator. You must answer only in a markdown code snippets. Use Code comments for explanations."
+}
+
+export async function POST(
+  req: Request
+){
+  try {
+
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId){
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("OpenAI API Key not configured", { status: 500 })
+    }
+
+    if (!messages){
+      return new NextResponse("Messages are required", { status: 400 })
+    }
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages]
+    });
+
+    return NextResponse.json(response.data.choices[0].message);
+
+  } catch (err: any){
+    console.log("[CODE_ERROR]", err?.message);
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
